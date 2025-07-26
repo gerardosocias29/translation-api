e86b695ea8f2365be78908f8a812e5baff734d2f
Gerardo <g.socias29@gmail.com>
2025-07-26 13:39:30 +0800

    added translations table and model

---
+++ app/Models/Translation.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Translation extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'group',
+        'key',
+        'value',
+        'locale',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'value' => 'string',
+            'locale' => 'string',
+        ];
+    }
+    
+}
