ced1f6cb398b4398a4390ac0acf031ba33cad7f5
Gerardo <g.socias29@gmail.com>
2025-07-26 13:07:23 +0800

    added default test user via UserSeed

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'gerardo@example.com'],
+            [
+                'name' => 'Gerardo',
+                'password' => Hash::make('password'),
+            ]
+        );
+    }
+}
